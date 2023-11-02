38e043ed0a969f233712958b9b502761b3a0b5fb
이수민 <lsmmuseum@gmail.com>
2023-11-03 08:36:33 +0900

    11/3

--- Cutting.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Collections;
-
-
-public class Cutting {
-    
-}
--- Main2.java
+++ Main2.java
@@ -2,30 +2,30 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
 
-
-public class Main {
+public class Main2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int K = input.nextInt();  
-        int N = input.nextInt(); 
-        ArrayList<Integer> lines = new ArrayList<>();
-        for (int i = 0; i < K; i++) {
-            lines.add(input.nextInt()); 
+        int N = input.nextInt();
+        int K = input.nextInt();
+        
+        ArrayList<Integer> churros = new ArrayList<>(); 
+        for (int i = 0; i < N; i++) {
+            churros.add(input.nextInt()); 
         }
-        Collections.sort(lines);
+        Collections.sort(churros); 
 
         if (K == 1) {
-            System.out.println(lines.get(lines.size() - 1));
-            System.exit(0);
+            System.out.println(churros.get(churros.size() - 1)); 
+            System.exit(0); 
         }
         int start = 1;
-        int end = lines.get(lines.size() - 1);
+        int end = churros.get(churros.size() - 1);
         while (start <= end) {
             boolean tof = false;
             int mid = (start + end) / 2;
             int res = 0;
             for (int i = 0; i < N; i++) {
-                res += lines.get(i) / mid;
+                res += churros.get(i) / mid;
                 if (res >= K) {
                     start = mid + 1;
                     tof = true;
